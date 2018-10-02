53c1a123aa4bf7e6cde9e6e8954ca8cb8c959ad2
WIN10 <hyunddu0824@gmail.com>
2018-10-02 13:23:13 +0900

    asdvzcvzx

--- example/src/main/java/com/spring/client/board/controller/BoardController.java
+++ example/src/main/java/com/spring/client/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -73,4 +74,7 @@ public class BoardController {
 		model.addAttribute("detail", detail);
 		return "board/boardDetail";
 	}
+	
+	/*public String boardInsert(BoardVO bvo, Model model, HttpServletRequest request)*/
+	
 }
